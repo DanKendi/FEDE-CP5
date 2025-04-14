e7bfcf76ca29022956eb4fef433fb9551f34d6df
DanKendi <dan.kendisa@gmail.com>
2025-04-13 23:48:06 -0300

    Adicionando comentarios finais

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import CardProduto from "@/components/CardProduto";
-import Footer from "@/components/Footer";
+import CardProduto from "@/components/CardProduto";//Importa componente CardProduto
+import Footer from "@/components/Footer"; //Importa componente Footer
 
 export default function Home() {
   return (
     <>
+      {/* Renderiza o componente CardProduto*/}
       <CardProduto />
+      {/* Renderiza o componente Footer*/}
       <Footer />
     </>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,11 @@
 function Footer() {
     return (
         <>
+            {/*Elemento <footer> com cor de fundo personalizada*/}
             <footer style={{backgroundColor: "#e0eeff"}}>
+                {/*Centraliza o conteúdo do rodapé*/}
                 <div style={{textAlign: "center", margin: "0 auto"}}>
+                    {/*Exibe o ano atual dinamicamente + texto de direitos autorais*/}
                     <p>{new Date().getFullYear()} &copy; Lista de Produtos. Daniel Kendi Todos os Direitos reservados.</p>
                 </div>
             </footer>
